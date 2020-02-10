3eef0f25f6ffd55de1ace4b292ae9cfedecb43ef
Linus Henze <LinusHenze@users.noreply.github.com>
2020-02-10 02:52:39 +0100

    install.sh/dlopen_in_pid

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,4 +1,66 @@
 #!/bin/bash
 
-echo "Nothing right now..."
-exit -1
+set -e
+
+EXE=$(basename "$0")
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $EXE <noRespring/respringNow>"
+    exit -5
+fi
+
+if [ "$1" = "noRespring" ]; then
+    # Stage 1
+    echo "$EXE: Stage 1, not respringing"
+    
+    echo "$EXE: Generating SSH Host Keys..."
+    /usr/bin/ssh-keygen -A
+    
+    echo "$EXE: Installing firmware package..."
+    /bin/bash /usr/libexec/cydia/firmware.sh
+    
+    echo "$EXE: Installing debs..."
+    cd /debs
+    /usr/bin/dpkg -i *
+    
+    echo "$EXE: Adding chimera repo..."
+    echo "deb https://repo.chimera.sh/ ./" > /etc/apt/sources.list.d/chimera.list
+    
+    echo "$EXE: Launching services..."
+    cd /Library/LaunchDaemons
+    /sbin/launchctl load *.plist
+    
+    echo "$EXE: Running uicache..."
+    /usr/bin/uicache --path /Applications/Sileo.app
+    
+    echo "$EXE: Stage 1 done"
+    
+    exit 0
+elif [ "$1" = "respringNow" ]; then
+    # Stage 2
+    echo "$EXE: Stage 2, will respring"
+    
+    echo "$EXE: Launching substitute..."
+    /etc/rc.d/substitute
+    
+    echo "$EXE: Injecting Unsandbox..."
+    PID=$(/sbin/launchctl list com.apple.cfprefsd.xpc.daemon | /usr/bin/grep PID | /usr/bin/xargs | /bin/sed -e "s/PID = //" -e "s/;//")
+    /System/Library/Substitute/Helpers/dlopen_in_pid $PID /System/Library/Substitute/Helpers/bundle-loader.dylib
+    
+    echo "$EXE: Respringing..."
+    /usr/bin/killall -SIGTERM SpringBoard
+    
+    echo "$EXE: Install completed"
+    echo "$EXE: Cleaning up..."
+    /bin/rm -rf /debs /bootstrap.tar /install.sh
+    
+    echo "$EXE: Creating /.Fugu_installed ..."
+    /usr/bin/touch /.Fugu_installed
+    
+    echo "$EXE: Stage 2 done"
+    exit 0
+else
+    # WTF?
+    echo "$EXE: Unknow argument $1"
+    exit -5
+fi
